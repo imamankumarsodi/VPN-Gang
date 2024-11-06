a199368aeddf2d253e4bc0391146437cde63fbd4
अमन कुमार <amankumarsodi@gmail.com>
2024-11-06 21:51:19 +0530

    Initial commit

---
+++ VPN Gang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VPN Gang
+//
+//  Created by Aman Kumar on 06/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("INITIAL SETUP...!")
+    }
+
+
+}
+
